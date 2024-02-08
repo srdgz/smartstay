da19af6b640134756a3431c333128f4bc9d2abc7
srdgz <sandra.rr@icloud.com>
2024-02-08 18:19:23 +0100

    sanity bug fixed

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -2,7 +2,9 @@ import { NextAuthOptions } from "next-auth";
 import { SanityAdapter, SanityCredentials } from "next-auth-sanity";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+
 import sanityClient from "./sanity";
+import { SanityClient } from "sanity";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -14,22 +16,30 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    SanityCredentials(sanityClient),
+    SanityCredentials(sanityClient as SanityClient),
   ],
   session: {
     strategy: "jwt",
   },
-  adapter: SanityAdapter(sanityClient),
+  adapter: SanityAdapter(sanityClient as SanityClient),
   debug: process.env.NODE_ENV === "development",
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     session: async ({ session, token }) => {
       const userEmail = token.email;
       const userIdObj = await sanityClient.fetch<{ _id: string }>(
-        `*[_type == "user" && email == $email] [0] {_id}`,
+        `*[_type == "user" && email == $email][0] {
+            _id
+        }`,
         { email: userEmail }
       );
-      return { ...session, user: { ...session.user, id: userIdObj._id } };
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: userIdObj._id,
+        },
+      };
     },
   },
 };
