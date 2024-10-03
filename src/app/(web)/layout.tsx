fb0e6cd52e4b7a22f3b43b4ca67fddcc9219063a
srdgz <sandra.rr@icloud.com>
2024-10-03 16:45:03 +0200

    fix: change icon

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/png" href="/icon.png" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
